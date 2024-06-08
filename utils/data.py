a21c0d85c589709aeeb881937c66bc926ee18ab7
kumudlakara <lakara.kumud@gmail.com>
2024-06-08 15:39:45 +0100

    update for returning annotations

--- utils/data.py
+++ utils/data.py
@@ -5,7 +5,7 @@ def get_data(i):
     visual_news_data = json.load(open("../../datasets/visualnews/origin/data.json"))
     visual_news_data_mapping = {ann["id"]: ann for ann in visual_news_data}
 
-    data = json.load(open("../../news_clippings/news_clippings/data/merged_balanced/val.json"))
+    data = json.load(open("../../news_clippings/news_clippings/data/merged_balanced/test.json"))
     annotations = data["annotations"]
     ann_true = annotations[i]
 
@@ -16,13 +16,13 @@ def get_data(i):
     #print("DATA SAMPLE")
     #print("Caption: ", caption)
     #print("Misinformation (Ground Truth): {}".format(ann_true["falsified"]))
-    return image, caption, image_path
+    return image, caption, image_path, ann_true
 
 def show_data(i):
     visual_news_data = json.load(open("../../datasets/visualnews/origin/data.json"))
     visual_news_data_mapping = {ann["id"]: ann for ann in visual_news_data}
 
-    data = json.load(open("../../news_clippings/news_clippings/data/merged_balanced/val.json"))
+    data = json.load(open("../../news_clippings/news_clippings/data/merged_balanced/test.json"))
     annotations = data["annotations"]
     ann_true = annotations[i]
 
