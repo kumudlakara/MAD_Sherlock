1619d111ca839100d90426560dff065ea3626596
kumudlakara <lakara.kumud@gmail.com>
2024-06-25 11:20:08 +0100

    add function for compiling results including model queries and search results

--- utils/compile_results.py
+++ utils/compile_results.py
@@ -15,3 +15,22 @@ def add_result(file_path, annotation):
     # Write the updated list to the JSON file
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
+
+
+def add_result_with_disambiguation(file_path, annotation, search_results, queries):
+    try:
+        # Read the existing file
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        # If the file doesn't exist or is empty, initialize an empty list
+        data = []
+
+    # Append the new result to the list
+    data.append(annotation)
+    data.append(search_results)
+    data.append(queries)
+
+    # Write the updated list to the JSON file
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
